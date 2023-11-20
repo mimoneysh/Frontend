0eb5ae0c2f778440d6fe488cf711086a8a7d292f
mimoneysh <manishapotikar4174@gmail.com>
2023-11-20 17:38:02 +0530

    20nov 17.37

---
+++ Javascript/lunch.js
@@ -0,0 +1,15 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    var draw=names[Math.floor(Math.random() * names.length)];
+    return draw +    " is going to buy lunch today!";
+    
+    
+    
+    
+
+
+/******Don't change the code below*******/    
+}
