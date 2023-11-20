823c50ec96e044d9d06d8451af0097131d876e30
mimoneysh <manishapotikar4174@gmail.com>
2023-11-20 14:48:56 +0530

    20nov 14.48

---
+++ Javascript/Lcalc.js.js
@@ -0,0 +1,7 @@
+prompt("Your name:");
+prompt("Your partners name:");
+
+var luvScore=Math.random()*100;
+luvScore=Math.floor(luvScore)+1;
+console.log(luvScore);
+alert("Your love Score is "+luvScore+"%");
