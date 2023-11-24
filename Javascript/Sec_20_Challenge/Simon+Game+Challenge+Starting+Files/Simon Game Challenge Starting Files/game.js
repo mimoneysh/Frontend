bcee254778ecf65277b238974f8688957c5cbe08
mimoneysh <manishapotikar4174@gmail.com>
2023-11-24 12:06:02 +0530

    24 nov 12.05

--- Javascript/Sec_20_Challenge/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Javascript/Sec_20_Challenge/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,11 @@
-alert("Welcome to simon..!Remember flashing pattern to score.")!
+alert("Welcome to simon..!\nRemember flashing pattern to score.");
+var gamePattern =[];
+var buttonColours =["red", "blue", "green", "yellow"];
+
+var randomNumber=Math.floor(Math.random()*4);
+console.log(randomNumber);
+
+var randomChosenColour = buttonColours[randomNumber];
+console.log(randomChosenColour);
+
+gamePattern.push(randomChosenColour);
