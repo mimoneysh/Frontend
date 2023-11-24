ed824823c9cd204cae9e63885a766241f3e06fdf
mimoneysh <manishapotikar4174@gmail.com>
2023-11-24 13:54:12 +0530

    audio done 24 nov 13.54

--- Javascript/Sec_20_Challenge/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Javascript/Sec_20_Challenge/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,4 @@
-alert("Welcome to simon..!\nRemember flashing pattern to score.");
+//alert("Welcome to simon..!\nRemember flashing pattern to score.");
 var gamePattern =[];
 var buttonColours =["red", "blue", "green", "yellow"];
 
@@ -8,4 +8,12 @@ console.log(randomNumber);
 var randomChosenColour = buttonColours[randomNumber];
 console.log(randomChosenColour);
 
-gamePattern.push(randomChosenColour);
+//var audio = $(selectButton.play());
+$('#'+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+audio.play();
+
+
+// gamePattern.push(randomChosenColour);
+// var selectButton =$('#'+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
